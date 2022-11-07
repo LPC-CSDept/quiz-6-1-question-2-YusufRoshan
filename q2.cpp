350ad086c583e6a64c01e58657c44b811ebec0d3
YusufRoshan <112139503+YusufRoshan@users.noreply.github.com>
2022-11-07 22:06:57 +0000

    #2

--- q2.cpp
+++ q2.cpp
@@ -6,9 +6,22 @@ void getInput(int &n1, int &n2, int &n3);
 
 int  findMin(int n1, int n2, int n3); 
 
+if(n1<n2 && n1<n3)
+	return n1;
+else if(n2<n3)
+	return 2;
+else 
+	return n3; 
+
 void printResult(int n1, int n2, int n3, int min);
 
+	cout << "Num1 : " << n1<< endl; 
+	cout << "Num2: " << n2 << endl;
+	cout << "Num3: " << n3 << endl;
+	cout << "Minimum is: " << min << endl;
+
 int main(){ 
+int n1, n2, n3, min; 
 getInput(n1, n2, n3);
 min = findMin(n1, n2, n3);
 printResult(n1, n2, n3, min);
