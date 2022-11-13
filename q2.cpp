5879c94c46778db9a9fc5726f144508963ca4c9c
Kyuwoong Lee <kwooler@gmail.com>
2022-11-13 15:09:52 -0800

    Update q2.cpp

--- q2.cpp
+++ q2.cpp
@@ -28,10 +28,10 @@ void printResult(int n1, int n2, int n3, int min){
 	cout << "Minimum is: " << min << endl;
 } 
 
-// int main(){ 
-// int n1, n2, n3, min; 
-// getInput(n1, n2, n3);
-// min = findMin(n1, n2, n3);
-// printResult(n1, n2, n3, min);
+int main(){ 
+int n1, n2, n3, min; 
+getInput(n1, n2, n3);
+min = findMin(n1, n2, n3);
+printResult(n1, n2, n3, min);
 
-// } 
+} 
