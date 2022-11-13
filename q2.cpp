42da6da005d3c2dfa59844731cb19cce2ef74159
Kyuwoong Lee <kwooler@gmail.com>
2022-11-13 15:07:14 -0800

    Update q2.cpp

--- q2.cpp
+++ q2.cpp
@@ -2,7 +2,7 @@
 #include <iomanip>
 using namespace std;
 
-void getRandnum(int &n1, int &n2, int &n3); {
+void getRandnum(int &n1, int &n2, int &n3) {
 	srand((unsigned int)time(NULL));
 	n1=rand()%100;
 	n2=rand()%100;
@@ -10,7 +10,7 @@ void getRandnum(int &n1, int &n2, int &n3); {
 }
 
 
-int  findMin(int n1, int n2, int n3);{
+int  findMin(int n1, int n2, int n3){
 
 if(n1<n2 && n1<n3)
 	return n1;
@@ -20,7 +20,7 @@ else
 	return n3; 
 } 
 
-void printResult(int n1, int n2, int n3, int min);{
+void printResult(int n1, int n2, int n3, int min){
 
 	cout << "Num1 : " << n1<< endl; 
 	cout << "Num2: " << n2 << endl;
@@ -28,10 +28,10 @@ void printResult(int n1, int n2, int n3, int min);{
 	cout << "Minimum is: " << min << endl;
 } 
 
-int main(){ 
-int n1, n2, n3, min; 
-getInput(n1, n2, n3);
-min = findMin(n1, n2, n3);
-printResult(n1, n2, n3, min);
+// int main(){ 
+// int n1, n2, n3, min; 
+// getInput(n1, n2, n3);
+// min = findMin(n1, n2, n3);
+// printResult(n1, n2, n3, min);
 
 } 
